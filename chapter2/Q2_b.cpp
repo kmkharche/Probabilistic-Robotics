43722db4391c06f3c2591f64958c4db14b3f8d89
Ketan Kharche <ketan3393@gmail.com>
2017-09-10 21:49:55 -0700

    feat: Question 2b of chapter 2

---
+++ chapter2/Q2_b.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+/*
+	Pss=P(Day2=sunny|Day1=sunny)
+	Pcs=P(Day2=cloudy|Day1=sunny)
+	Prs=P(Day2=rainy|Day1=sunny)
+
+	Similarly for the other cases
+*/
+
+double getRandomNumber(int min, int max)
+{
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);  // static used for efficiency, so we only calculate this value once
+	// evenly distribute the random number across our range
+	return (rand() * fraction * (max - min) + min);
+}
+
+int main()
+{
+
+	srand(static_cast<unsigned int>(time(0)));
+
+	double Pss = 0.8;
+	double Pcs = 0.2;
+	double Prs = 0;
+	double Psc = 0.4;
+	double Pcc = 0.4;
+	double Prc = 0.2;
+	double Psr = 0.2;
+	double Pcr = 0.6;
+	double Prr = 0.2;
+
+	int day = 0;
+	while (day<=0)
+	{
+		cout << "Enter the number of days for which you want the weather prediction: ";
+		cin >> day;
+	}
+
+	//If day1 is sunny
+	double Ps=1;
+	double Pc=0;
+	double Pr=0;
+
+	cout << "sunny ";
+
+	for (int i=1;i<day;++i)
+	{
+		double PsNew = Pss*Ps + Psc*Pc + Psr*Pr;
+		double PcNew = Pcs*Ps + Pcc*Pc + Pcr*Pr;
+		double PrNew = Prs*Ps + Prc*Pc + Prr*Pr;
+
+		Ps = PsNew;
+		Pc = PcNew;
+		Pr = PrNew;
+
+		double sample = getRandomNumber(0,1);
+		cout << sample << endl;
+		if (sample < Ps)
+			cout << "sunny ";
+		else if(sample < (Ps+Pc))
+			cout << "cloudy ";
+		else
+			cout << "rainy ";
+
+	}
+	
+	cout << endl;
+
+	//Standard Distribution
+	///For standard distribution i.e. steady state,  use a large number of days
+	day = 10000;
+
+	for (int i=1;i<day;++i)
+	{
+		double PsNew = Pss*Ps + Psc*Pc + Psr*Pr;
+		double PcNew = Pcs*Ps + Pcc*Pc + Pcr*Pr;
+		double PrNew = Prs*Ps + Prc*Pc + Prr*Pr;
+
+		Ps = PsNew;
+		Pc = PcNew;
+		Pr = PrNew;
+	}
+	cout << "The standard distribution probability that a day is sunny is " << Ps << "." << endl;
+	cout << "The standard distribution probability that a day is cloudy is " << Pc << "." << endl;
+	cout << "The standard distribution probability that a day is rainy is " << Pr << "." << endl;
+
+	return 0;
+}
